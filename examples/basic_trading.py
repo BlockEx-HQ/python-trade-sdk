31ce2f36e670777e88e791c3fc1d5febfa335fbb
Alex Kotenko <alex.kotenko@blockex.com>
2018-04-10 12:00:14 +0100

    Make integration tests also pass, needs correct user params

--- examples/basic_trading.py
+++ examples/basic_trading.py
@@ -1,27 +1,26 @@
 from blockex.tradeapi.tradeapi import BlockExTradeApi
-from blockex.tradeapi.tradeapi import OrderType
-from blockex.tradeapi.tradeapi import OfferType
+from blockex.tradeapi import C
 
 
 def main():
-    # The API URL, e.g. https://api.blockex.com/
-    api_url = ''
+    # The API URL, defaults to https://api.blockex.com/
+    api_url = None
 
-    # The Partner's API ID
-    api_id = ''
+    # The Partner's API ID, defaults to 7c11fb8e-f744-47ee-aec2-9da5eb83ad84 (blockexmarkets.com)
+    api_id = None
 
-    # The Trader''s username
+    # The Trader''s username, must be provided
     username = ''
 
-    # The Trader''s password
+    # The Trader''s password, must be provided
     password = ''
 
     try:
         # Create Trade API instance
-        trade_api = BlockExTradeApi(api_url=api_url,
-                                    api_id=api_id,
-                                    username=username,
-                                    password=password)
+        trade_api = BlockExTradeApi(username=username,
+                                    password=password,
+                                    api_url=api_url,
+                                    api_id=api_id,)
 
         # Get trader instruments
         trader_instruments = trade_api.get_trader_instruments()
@@ -34,27 +33,27 @@ def main():
 
         # Get trader orders filtered
         orders_filtered = trade_api.get_orders(instrument_id=1,
-                                               order_type=OrderType.LIMIT,
-                                               offer_type=OfferType.BID,
-                                               status='10,20',
+                                               order_type=C.OrderType.LIMIT,
+                                               offer_type=C.OfferType.BID,
+                                               status=[C.OrderStatus.PENDING, C.OrderStatus.PLACED],
                                                load_executions=True,
                                                max_count=5)
 
         # Get market orders unfiltered
-        market_orders = trade_api.get_market_orders(instrument_id=1)
+        market_orders = trade_api.get_market_orders(instrument_id=trader_instruments[0]['id'])
 
         # Get market orders filtered
         market_orders_filtered = trade_api.get_market_orders(
             instrument_id=1,
-            order_type=OrderType.LIMIT,
-            offer_type=OfferType.BID,
-            status='10,20',
+            order_type=C.OrderType.LIMIT,
+            offer_type=C.OfferType.BID,
+            status=[C.OrderStatus.PENDING, C.OrderStatus.PLACED],
             max_count=5)
 
         # Place order
-        trade_api.create_order(offer_type=OfferType.BID,
-                               order_type=OrderType.LIMIT,
-                               instrument_id=1,
+        trade_api.create_order(offer_type=C.OfferType.BID,
+                               order_type=C.OrderType.LIMIT,
+                               instrument_id=trader_instruments[0]['id'],
                                price=5.2,
                                quantity=1)
 
@@ -62,7 +61,7 @@ def main():
         trade_api.cancel_order(order_id=32598)
 
         # Cancel all orders for an instrument
-        trade_api.cancel_all_orders(instrument_id=1)
+        trade_api.cancel_all_orders(instrument_id=trader_instruments[0]['id'])
 
         # The following scenario gets all the instruments available for the trader and iterates them.
         # For each instrument all orders are cancelled at first. Then the best bid and ask prices are
@@ -72,21 +71,31 @@ def main():
 
         for instrument in trader_instruments:
             try:
-                instrument_id = instrument['id']
+                trade_api.cancel_all_orders(instrument['id'])
 
-                trade_api.cancel_all_orders(instrument_id)
-
-                ask_market_orders = trade_api.get_market_orders(instrument_id, OrderType.MARKET, OfferType.BID, '20,50', 1000)
+                ask_market_orders = trade_api.get_market_orders(instrument['id'],
+                                                                C.OrderType.MARKET,
+                                                                C.OfferType.BID,
+                                                                [C.OrderStatus.PLACED,
+                                                                 C.OrderStatus.PARTEXECUTED,],
+                                                                1000)
                 if ask_market_orders:
                     ask_market_orders_prices = [order['price'] for order in ask_market_orders]
                     best_bid_price = min(ask_market_orders_prices)
-                    trade_api.create_order(OfferType.BID, OrderType.LIMIT, instrument_id, best_bid_price, 1)
+                    trade_api.create_order(C.OfferType.BID, C.OrderType.LIMIT,
+                                           instrument['id'], best_bid_price, 1)
 
-                bid_market_orders = trade_api.get_market_orders(instrument_id, OrderType.MARKET, OfferType.BID, '20,50', 1000)
+                bid_market_orders = trade_api.get_market_orders(instrument['id'],
+                                                                C.OrderType.MARKET,
+                                                                C.OfferType.BID,
+                                                                [C.OrderStatus.PLACED,
+                                                                 C.OrderStatus.PARTEXECUTED,],
+                                                                1000)
                 if bid_market_orders:
                     bid_market_orders_prices = [order['price'] for order in bid_market_orders]
                     best_ask_price = max(bid_market_orders_prices)
-                    trade_api.create_order(OfferType.ASK, OrderType.LIMIT, instrument_id, best_ask_price, 1)
+                    trade_api.create_order(C.OfferType.ASK, C.OrderType.LIMIT,
+                                           instrument['id'], best_ask_price, 1)
             except Exception as err:
                 print(err)
     except Exception as err:
